b42248072efc0e90c139ce7f77545e58c67ed15e
fhernanb <fhernanb@unal.edu.co>
2021-03-23 20:22:42 -0500

    cambio el color

--- figures/r_code_hexalogo.R
+++ figures/r_code_hexalogo.R
@@ -7,7 +7,7 @@ library(hexSticker)
 s <- sticker(subplot="figures/tijeras.png",
              package="GHQp",
              p_color="gray66",
-             h_color="slateblue",
+             h_color="mediumpurple4",
              p_size=25, 
              s_x=1.1, s_y=0.7, 
              s_width=0.6, s_height=0.4,
